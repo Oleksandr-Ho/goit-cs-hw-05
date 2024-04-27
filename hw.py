a31ec8a52a8b9c0cd9eda361913fcb1a19693dea
Oleksandr-Ho <holodetskiy@gmail.com>
2024-04-28 02:21:15 +0300

    Initial commit

--- hw.py
+++ hw.py
@@ -5,32 +5,26 @@
 топ-слова з найвищою частотою використання у тексті.
 """
 
-
 import string
-import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from collections import defaultdict, Counter
-
-import httpx
 from matplotlib import pyplot as plt
+import requests
 
-
-async def get_text(url):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code == 200:
-            return response.text
-        else:
-            return None
-
+def get_text(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Перевірка на помилки HTTP
+        return response.text
+    except requests.RequestException as e:
+        return None
 
 # Функція для видалення знаків пунктуації
 def remove_punctuation(text):
     return text.translate(str.maketrans("", "", string.punctuation))
 
-
-async def map_function(word) -> tuple:
+def map_function(word):
     return word, 1
-
 
 def shuffle_function(mapped_values):
     shuffled = defaultdict(list)
@@ -38,55 +32,52 @@ def shuffle_function(mapped_values):
         shuffled[key].append(value)
     return shuffled.items()
 
-
-async def reduce_function(key_values):
+def reduce_function(key_values):
     key, values = key_values
     return key, sum(values)
 
-
 # Виконання MapReduce
-async def map_reduce(text, search_words=None):
-    text = await get_text(url)
-    if text:
-        # Видалення знаків пунктуації
-        text = remove_punctuation(text)
-        words = text.split()
+def map_reduce(text, search_words=None):
+    # Видалення знаків пунктуації
+    text = remove_punctuation(text)
+    words = text.split()
 
-        # Якщо задано список слів для пошуку, враховувати тільки ці слова
-        if search_words:
-            words = [word for word in words if word in search_words]  # filter
+    # Якщо задано список слів для пошуку, враховувати тільки ці слова
+    if search_words:
+        words = [word for word in words if word in search_words]
 
-        # Паралельний Мапінг
-        mapped_values = await asyncio.gather(*[map_function(word) for word in words])
+    # Паралельний Мапінг
+    with ThreadPoolExecutor() as executor:
+        mapped_values = list(executor.map(map_function, words))
 
-        # Крок 2: Shuffle
-        shuffled_values = shuffle_function(mapped_values)
+    # Крок 2: Shuffle
+    shuffled_values = shuffle_function(mapped_values)
 
-        # Паралельна Редукція
-        reduced_values = await asyncio.gather(*[reduce_function(key_values) for key_values in shuffled_values])
+    # Паралельна Редукція
+    with ThreadPoolExecutor() as executor:
+        reduced_values = list(executor.map(reduce_function, shuffled_values))
 
-        return dict(reduced_values)
-    else:
-        return None
-    
-def visualize_top_words(result):
-    top_10 = Counter(result).most_common(10)
-    labels, values = zip(*top_10)
+    return dict(reduced_values)
+
+# Функція для візуалізації
+def visualize_top_words(result, top_n=10, bar_color='blue'):
+    top_words = Counter(result).most_common(top_n)
+    words, frequencies = zip(*top_words)
     plt.figure(figsize=(10, 5))
-    plt.barh(labels, values, color='green')
+    plt.barh(words, frequencies, color=bar_color)
     plt.xlabel('Частота')
     plt.ylabel('Слова')
-    plt.title('Топ 10 найчастіше вживаних слів')
+    plt.title('Топ {} найчастіше вживаних слів'.format(top_n))
     plt.gca().invert_yaxis()
     plt.show()
 
-
 if __name__ == '__main__':
-    # Вхідний текст для обробки
-    url = "https://gutenberg.net.au/ebooks01/0100021.txt"
-    # Виконання MapReduce на вхідному тексті
-    search_words = None  # Наприклад: ['brother', 'big', 'hate', 'peace']
-    result = asyncio.run(map_reduce(url, search_words))
-
-    print("Результат підрахунку слів:", result)
-    visualize_top_words(result)
+    url = "https://gutenberg.net.au/ebooks01/0100021.txt" 
+    text = get_text(url)
+    if text:
+        search_words = None  # Можна вказати конкретні слова для пошуку
+        result = map_reduce(text, search_words)
+        print("Результат підрахунку слів:", result)
+        visualize_top_words(result)
+    else:
+        print("Помилка: Не вдалося отримати вхідний текст.")
